3d9f4a14aab4db757eb955c3e5f441cec320a104
Cragady <pinkdotdreamer@gmail.com>
2021-03-26 17:21:45 -0600

    Updated basic layouts for Therapy component, updated some functionality of CmsAPI

--- react-client/src/components/Therapy/Therapy.js
+++ react-client/src/components/Therapy/Therapy.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { CmsAPI, UrlParser } from '../../utils';
+import './Therapy.css';
 
 export class Therapy extends Component {
 
+    state = {
+        therapy: []
+    }
+
+    componentDidMount() {
+        let url_id = UrlParser.getIdFromUrl(window.location.pathname);
+        CmsAPI.getTherapy(url_id)
+            .then(res => {
+                this.setState({
+                    therapy: [res.data]
+                })
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
     render() {
-        return (
-            <section>
-                <h1>Hello, World! The Therapy component says 'Hello!'</h1>
+        let therapy_pass = this.state.therapy.length > 0,
+            therapy_state = this.state.therapy[0],
+            therapy_component = null;
+
+        if(therapy_pass) {
+            const { date_available, description, hourly_price, name } = therapy_state;
+            therapy_component = <section className="Therapy-section pt-3">
+                <div className="container rounded Therapy-container">
+                    <h1>{name}</h1>
+                    <h2>Description</h2>
+                    <p className="Therapy-description rounded p-3">{description}</p>
+                    <p><span className="Therapy-point-text">Hourly Price:</span> {hourly_price}</p>
+                    <p><span className="Therapy-point-text">Available:</span> {date_available}</p>
+                    <button className="btn btn-primary Therapy-btn mb-3">Add to Cart</button>
+                </div>
             </section>
-        )
+        }
+
+
+        return ( therapy_component )
     }
 }
 
--- react-client/src/utils/CmsAPI.js
+++ react-client/src/utils/CmsAPI.js
@@ -4,11 +4,21 @@ export const CmsAPI = {
 
     CmsUrl: process.env.REACT_APP_CMS_API,
 
+    therapy: '/fast-food-version-of-therapies',
+
     getProducts() {
         return axios.get(`${this.CmsUrl}/products`)
     },
 
     getProduct(id) {
         return axios.get(`${this.CmsUrl}/products/` + id)
+    },
+
+    getTherapies() {
+        return axios.get(this.CmsUrl + this.therapy)
+    },
+
+    getTherapy(id) {
+        return axios.get(this.CmsUrl + this.therapy + '/' + id)
     }
 }
