b6e410f87fab587b17f421070dd8a22a6aefba24
Cragady <pinkdotdreamer@gmail.com>
2021-03-26 17:00:27 -0600

    More setting up Product component and styling

--- react-client/src/components/Product/Product.js
+++ react-client/src/components/Product/Product.js
@@ -28,14 +28,16 @@ export class Product extends Component {
 
             if(product_pass) {
                 const { name, description, price, quantity, picture } = product_state;
-                product_component = <section className="Product-section">
-                    <h1>{name}</h1>
-                    <img className="Product-img img-fluid mb-3" src={CmsAPI.CmsUrl + picture[0].url} alt={"Product image " + name} />
-                    <h2>Description</h2>
-                    <p>{description}</p>
-                    <p>Price: {price}</p>
-                    <p>Available: {quantity}</p>
-                    <button className="btn btn-primary Product-btn">Add to Cart</button>
+                product_component = <section className="Product-section pt-3">
+                    <div className="container rounded Product-container">
+                        <h1>{name}</h1>
+                        <img className="Product-img img-fluid mb-3" src={CmsAPI.CmsUrl + picture[0].url} alt={"Product image " + name} />
+                        <h2>Description</h2>
+                        <p className="Product-description rounded p-3">{description}</p>
+                        <p><span className="Product-point-text">Price:</span> {price}</p>
+                        <p><span className="Product-point-text">Available:</span> {quantity}</p>
+                        <button className="btn btn-primary Product-btn mb-3">Add to Cart</button>
+                    </div>
                 </section>
             }
 
